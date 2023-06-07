7ee7bf6b8820ebbb8498cc94fe099ecf5dcebcfc
Angel Georgiev <angelgeor0560@gmail.com>
2023-06-07 16:55:11 +0300

    Add files via upload

---
+++ SOLID/p02_OpenClosedPrinciple/p02_DrawingShape/Rectangle.java
@@ -0,0 +1,7 @@
+package Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape;
+
+import Solid_Lab.p02_OpenClosedPrinciple.p02_DrawingShape.interfaces.Shape;
+
+public class Rectangle implements Shape {
+
+}
